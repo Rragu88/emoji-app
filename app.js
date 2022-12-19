6207f4ac1a608f609e56f3e4a22ef25e94806584
Rragu88 <42756955+Rragu88@users.noreply.github.com>
2022-12-19 11:09:18 -0600

    created variables needed for app

---
+++ app.js
@@ -0,0 +1,7 @@
+let myEmojis = [];
+const emojiContainer = document.getElementById('emojiContainer');
+const emojiInput = document.getElementById('emoji-input');
+const pushBtn = document.getElementById('push-btn');
+const unshiftBtn = document.getElementById('unshift-btn');
+const popBtn = document.getElementById('pop-btn');
+const shiftBtn = document.getElementById('shift-btn');
