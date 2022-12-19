d1af44b0041f21b7fd83c98e15ac2ccde06887b2
Rragu88 <42756955+Rragu88@users.noreply.github.com>
2022-12-19 11:10:21 -0600

    created function to modify the emoji array

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ const pushBtn = document.getElementById('push-btn');
 const unshiftBtn = document.getElementById('unshift-btn');
 const popBtn = document.getElementById('pop-btn');
 const shiftBtn = document.getElementById('shift-btn');
+
+function modifyEmojiArray(type, arr) {
+    if (type === "push" && emojiInput.value) {
+        arr.push(emojiInput.value);
+    } else if (type === "unshift" && emojiInput.value) {
+        arr.unshift(emojiInput.value);
+    } else if (type === "pop") {
+        arr.pop();
+    } else if (type === "shift") {
+        arr.shift();
+    }
+    localStorage.setItem('myEmojis', JSON.stringify(arr));
+    emojiInput.value = '';
+    render(arr);
+}
